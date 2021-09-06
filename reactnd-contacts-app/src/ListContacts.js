38aad9b4529bc1eea403e7cbe0928314e916869d
Ebuka Peter <peterdbrainy5@gmail.com>
2021-09-06 22:19:33 +0100

    Changed all the class components to Functional components, and started adding state

--- reactnd-contacts-app/src/ListContacts.js
+++ reactnd-contacts-app/src/ListContacts.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListContacts extends Component {
-    render(){
-        return (
+
+const ListContacts = (props) => {
+    return (
            
-            <ol className="contact-list">
-            {
-             this.props.contacts.map(contact => 
-            <li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar"
-            style={{
-                backgroundImage: `url(${contact.avatarURL})`
-            }}>
-            
-            </div>
-            <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-            </div>
-            <button className="contact-remove">
-            Remove
-            </button>
-            </li>
-             
-            )
-        }
-            </ol>
+        <ol className="contact-list">
+        {
+         props.contacts.map(contact => 
+        <li key={contact.id} className="contact-list-item">
+        <div className="contact-avatar"
+        style={{
+            backgroundImage: `url(${contact.avatarURL})`
+        }}>
+        
+        </div>
+        <div className="contact-details">
+        <p>{contact.name}</p>
+        <p>{contact.handle}</p>
+        </div>
+        <button 
+        onClick={()=> props.onDeleteContact(contact)}
+        className="contact-remove">
+        Remove
+        </button>
+        </li>
+         
         )
     }
+        </ol>
+    )
 }
 
 export default ListContacts;
