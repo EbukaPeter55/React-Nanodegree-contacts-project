52add3ac76bc9e0eae1e801bf37d12b092e750a5
Ebuka Peter <peterdbrainy5@gmail.com>
2021-09-09 22:08:58 +0100

    Removed the array of contacts in the state, and fetched state from the backend using a ajax request in the componentDidMount life cycle

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -1,31 +1,23 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
-
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
   }
+  componentDidMount(){
+    ContactsAPI.getAll()
+    .then((contacts)=> {
+    console.log(contacts);
+
+    this.setState(()=> ({
+      contacts
+      
+    }))
+    })
+  } 
 
   removeContact = (contact) => {
   this.setState((currentState) => ({
@@ -33,6 +25,7 @@ class App extends Component {
       return c.id !== contact.id
     })
   }))
+  ContactsAPI.remove(contact);
   }
 
   render() {
